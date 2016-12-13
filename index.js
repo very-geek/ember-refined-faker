d602278ec19198dd2e0d1d3cc3346cce25600b39
Albert Yu <dev.yufan@gmail.com>
2016-12-13 20:11:49 +0800

    Add default configurations

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 'use strict'
 
+/**
+ * @module ember-refined-faker
+ */
+
+ /**
+ * @class config
+ */
+
 module.exports = {
   name: 'ember-refined-faker',
 
@@ -7,11 +15,11 @@ module.exports = {
     nodeAssets: {
       faker() {
         return {
-          import: [{
-            path: 'build/build/faker.js',
-            using: [{ transformation: 'amd', as: 'faker' }]
-          }],
-          enabled: this.isDevelopingAddon()
+          srcDir: 'build/build/',
+          import: [
+            { path: 'faker.js', using: [{ transformation: 'amd', as: 'faker' }] }
+          ],
+          enabled: this.__defaults.enabled
         }
       }
     }
@@ -19,5 +27,27 @@ module.exports = {
 
   isDevelopingAddon() {
     return !this.app.isProduction
+  },
+
+  /**
+   * Default configurations.
+   * Users can overwrite this in Ember Application's config/environment.js.
+   *
+   * @property defaults
+   * @type Object
+   * @param defaults.defaultLocale { String } 缺省使用的语言，默认值：'en_US'
+   * @param defaults.imageService { String } 占位图片使用的服务名称，默认值：'default'，使用 faker 内置的占位图片服务
+   * @param defaults.enabled { Boolean } 是否开启 ember-refined-faker，默认会在 production 下禁止
+   */
+ config(env, config) {
+    this.__defaults = {
+      defaultLocale: 'en_US',
+      imageService: 'default',
+      enabled: 'production' !== env
+    }
+
+    return {
+      faker: Object.assign(this.__defaults, config.faker || {})
+    }
   }
 }
